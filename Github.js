dd1deee0b43cb993a5dda464328d5d796e7c8b1d
nmmmmmn <995508233@qq.com>
2024-10-16 18:09:33 +0800

    更新 VSCO+++.js

--- Github.js
+++ Github.js
@@ -1,24 +1,43 @@
-/*
-项目名称：GitHub 1.142.1
-使用说明：除了AI功能，其他功能都可用
+/**************************
+ 
+*  * @Author: 🐈🐈🐈
+
+    \📷VSCO+++\
+
+***************************
 
 [rewrite_local]
-https://api.github.com/graphql url script-response-body https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/Github.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/VSCO+++.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/VSCO+++.js
 
-[mitm]
-hostname = api.github.com
+[MITM]
+hostname = api.revenuecat.com
 
-*/
+**************************/
 
-let obj = JSON.parse($response.body);
-if (obj.data && obj.data.viewer) {
-  obj.data.viewer.isProPlan = true;
-  obj.data.viewer.isEmployee = true;
-  obj.data.viewer.hasAppleIapSubscription = true;
-
-  $done({
-    body: JSON.stringify(obj)
-  });
-} else {
-  $done({});
+const jie = {};
+const lllll = JSON.parse(typeof $response != "undefined" && $response.body || null);
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  jie.headers = $request.headers;
+} else if (lllll && lllll.subscriber) {
+  lllll.subscriber.subscriptions = lllll.subscriber.subscriptions || {};
+  lllll.subscriber.entitlement = lllll.subscriber.entitlement || {};
+  const app = 'gd';const list = {'gd':{name: 'pro', id: 'vscopro_global_5999_annual_AutoFreeTrial'}};
+  const data = {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
+    "store" : "app_store"};
+for (const i in list) { if (new RegExp(`^${i}`, `i`).test(app)) {
+lllll.subscriber.subscriptions[list[i].id] = data;
+lllll.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+lllll.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+                break;
+          }
+    }
+    jie.body = JSON.stringify(lllll);
 }
+$done(jie);
