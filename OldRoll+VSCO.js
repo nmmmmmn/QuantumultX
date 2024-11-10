83d083c6a7cda1e4461ed51d589c248ad6dd4acc
nmmmmmn <995508233@qq.com>
2024-11-10 12:52:26 +0800

    OldRoll+VSCO.js

---
+++ OldRoll+VSCO.js
@@ -0,0 +1,17 @@
+/***************
+
+
+📷OldRoll+VSCO
+          
+
+
+[rewrite_local]
+
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/🌵OldRoll+++.js
+
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/VSCO+++.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/VSCO+++.js
+
+
+[mitm]
+hostname =  buy.itunes.apple.com, api.revenuecat.com
