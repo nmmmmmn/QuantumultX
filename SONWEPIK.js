956264f88f4b899b32b77c2b427de476be03845e
nmmmmmn <995508233@qq.com>
2024-12-20 18:18:14 +0800

    SONWEPIK.js

---
+++ SONWEPIK.js
@@ -0,0 +1,50 @@
+/*************************************
+
+
+
+[rewrite_local]
+^https?:\/\/.*\.snow\.me\/v\d\/purchase\/subscription\/subscriber\/status url script-response-body https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/SONWEPIK.js
+
+[mitm]
+hostname = *.snow.me
+
+*************************************/
+
+var Parr = JSON.parse($response.body);
+const ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+const times = Date.now();
+
+const list = {
+  "iphoneapp.epik": { id: "com.snowcorp.epik.subscribe.plan.oneyear" },  //
+  "iphoneapp.snow": { id: "com.campmobile.snow.subscribe.oneyear" }  //
+};
+
+for (const key of Object.keys(list)) {
+  if (new RegExp(`^${key}`, "i").test(ua)) {
+    Parr.result = {
+      "products": [
+        {
+          "managed": true,
+          "status": "ACTIVE",
+          "startDate": times,
+          "productId": list[key].id,
+          "expireDate": 4092599349000
+        }
+      ],
+      "tickets": [
+        {
+          "managed": true,
+          "status": "ACTIVE",
+          "startDate": times,
+          "productId": list[key].id,
+          "expireDate": 4092599349000
+        }
+      ],
+      "activated": true
+    };
+    console.log("🐈🐈🐈");
+    break;
+  }
+}
+
+$done({ body: JSON.stringify(Parr) });
