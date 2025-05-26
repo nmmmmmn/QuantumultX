b862868ab937dd55d7946a0aa520b12cc4931c85
nmmmmmn <995508233@qq.com>
2025-05-26 17:32:41 +0800

    .

--- R.js
+++ R.js
@@ -3,7 +3,7 @@
 
 [rewrite_local]
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/R.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/R.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/0OOOOOOOO/OOO/refs/heads/main/O.js
 
 [mitm]
 hostname = api.revenuecat.com
