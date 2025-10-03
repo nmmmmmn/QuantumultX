d0d3a5474f5b534bfd0c37ab4f27a26c3c980bef
nmmmmmn <995508233@qq.com>
2025-10-03 20:16:54 +0800

    更新 photography.js

--- photography.js
+++ photography.js
@@ -14,7 +14,7 @@
 
 ^https:\/\/photos\.adobe\.io\/v2\/accounts* url script-response-body https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/Lightroom+++.js
 
-^https?:\/\/.*oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/Focos+++.js
+^https?:\/\/.*\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/.+|purchases\/verify) url script-response-body https://raw.githubusercontent.com/nmmmmmn/QuantumultX/main/Focos+++.js
 
 ^https?:\/\/fimo\.appfimo\.com\/fimo-user\/user url script-response-body https://raw.githubusercontent.com/0l23456789/Quantumult-X/main/FIMOPRO.js
 
